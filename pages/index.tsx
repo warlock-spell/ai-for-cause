4d6bed2314b31cb64549ed0d5dc339a215fb5d59
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-11-16 19:33:19 +0530

    minor fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from "../components/Header"
 
-const Home: NextPage = () => {
+export default function Home(){
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -14,7 +13,7 @@ const Home: NextPage = () => {
       <div className='flex justify-between items-center bg-ai-logo-black border-y border-black py-10 lg:py-0'>
         <div className='px-10 space-y-5'>
           <h1 className='text-6xl text-ai-logo-text max-w-xl font-mono'>AI for <span className='text-ai-logo-header-yellow'>cause</span> - Exploring Endless Possibilities With AI.</h1>
-        <h2 className='text-ai-logo-text'>Making a difference with Artificial Intelligence</h2>
+        <h2 className='text-ai-logo-text'>Making a difference with Artificial Intelligence.</h2>
         </div>
         
         <img 
@@ -28,4 +27,3 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
