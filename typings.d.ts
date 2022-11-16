983e5f61d61668d76506f745ba4e635c7abc0f77
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-11-16 20:47:39 +0530

    define interface - post

---
+++ typings.d.ts
@@ -0,0 +1,20 @@
+// it is definition typescript file
+export interface Post {
+    _id: string;
+    _createdAt: string;
+    title: string;
+    author: {
+        name: string;
+        image: string;
+    };
+    description: string;
+    mainImage: {
+        asset: {
+            url: string;
+        };
+    };
+    slug: {
+        current: string;
+    };
+    body: [object];
+}
