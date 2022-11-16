67150e0be19b8f3112057a10de3b09f59f4789c5
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-11-16 10:29:46 +0530

    header banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,16 +3,35 @@ import NextImage from 'next/image'
 
 function Header() {
   return (
-    <header>
-      <div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-8">
         <Link href="/">
           <img 
           className="w-44 object-contain cursor-pointer" 
           src="/logoT.png"
           alt="" />
         </Link>
+        <div className="hidden md:inline-flex
+        items-center space-x-10">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          
+        </div>
       </div>
-      <div></div>
+      
+      <div className="flex items-center space-x-5 ">
+        <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            <Link href="https://github.com/warlock-spell">
+              GitHub
+            </Link>
+        </h3>
+        <h3 className="text-green-600 px-4 py-1 rounded-full border-green-600">
+            <Link href="https://daksh.wiki/">
+              Other Links
+            </Link>
+        </h3>
+
+      </div>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,26 @@ import Header from "../components/Header"
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>AI For Cause</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <div className='flex justify-between items-center bg-ai-logo-black border-y border-black py-10 lg:py-0'>
+        <div className='px-10 space-y-5'>
+          <h1 className='text-6xl text-ai-logo-text max-w-xl font-mono'>AI for <span className='text-ai-logo-header-yellow'>cause</span> - Exploring Endless Possibilities With AI.</h1>
+        <h2 className='text-ai-logo-text'>Making a difference with Artificial Intelligence</h2>
+        </div>
+        
+        <img 
+        className='hidden md:inline-flex h-32 lg:h-full'
+        src="/AI_4.png" alt="logo img" />
+
+      </div>
+
+      {/* Posts */}
     </div>
   )
 }
