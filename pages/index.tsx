fbfc1e3598beace45d9621170fe2e30d22283ffe
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-11-16 21:05:59 +0530

    implement SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import Head from 'next/head'
 import Header from "../components/Header"
+import { sanityClient, urlFor } from "../sanity"
+import { Post } from "../typings"
+import Link from "next/link"
 
-export default function Home(){
+// defining custom type "Props"
+interface Props{
+  // Post is defined in typings.d.ts
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props){
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -23,7 +32,30 @@ export default function Home(){
       </div>
 
       {/* Posts */}
+      
+
     </div>
   )
 }
 
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+        name,
+        image
+    },
+    description,
+    mainImage,
+    slug,
+  }`
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
