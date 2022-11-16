70a13564d9497d08d3f25ee82691276eb5f2a603
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-11-16 09:53:24 +0530

    extend theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ai-logo-black": "#272727",
+        "ai-logo-text": "#ece6db",
+        "ai-logo-header-yellow": "#ffde59",
+      },
+    },
   },
   plugins: [],
 }
