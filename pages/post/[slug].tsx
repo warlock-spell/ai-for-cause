ebf1c3a76d2bb5fe94a6a10c0fdb83ff88cee1d6
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-11-17 13:50:52 +0530

    link post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,77 @@
-function Post() {
+import Header from "../../components/Header"
+import { sanityClient, urlFor } from "../../sanity"
+import { Post } from "../../typings"
+import { GetStaticProps } from "next"
+
+interface Props {
+    post: Post
+}
+
+function Post({ post }: Props) {
+    console.log(post)
   return (
-    <div></div>
+    <main>
+        <Header />
+    </main>
   )
 }
 
 export default Post
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "post"]{
+        _id,
+        slug {
+        current
+        },
+    }`
+
+    const posts = await sanityClient.fetch(query)
+
+    const paths = posts.map((post: Post) => ({
+        params: {
+            slug: post.slug.current,
+        },
+    }))
+
+    return {
+        paths,
+        fallback: "blocking",
+    }
+}
+
+export const getStaticProps: GetStaticProps = async({params}) => {
+    const query = `*[_type == "post" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        title,
+        author -> {
+        name,
+        image,
+        },
+        description,
+        mainImage,
+        slug,
+        body,
+    }`
+
+    const post = await sanityClient.fetch(query, {
+        slug: params?.slug,
+    })
+
+    if (!post) {
+        return {
+            // returns 404 page
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            post,
+        },
+        // enables ISR
+        // after 60 seconds it will update the old cached version
+        revalidate: 60,
+    }
+}
