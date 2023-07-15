231caf53e46e881564ac9a72078455e824486487
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-07-15 13:44:28 +0530

    update link

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ function Header() {
           className="hidden md:inline-flex
         items-center space-x-10"
         >
-          <Link href="daksh.fyi">
+          <Link href="https://dakshgaur.com/">
             <h3>About Me</h3>
           </Link>
           <Link href="/contact">
